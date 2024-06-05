3e19f4c2cf3e4ffd9e61243a563c8278a1290d73
wisan <wisansilva3@gmail.com>
2024-06-05 15:30:49 -0300

    learning how to click on calendars with selenium

--- src/dropdowns/DynamicDropDown.java
+++ src/dropdowns/DynamicDropDown.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class DynamicDropDown {
 
@@ -20,8 +21,13 @@ public class DynamicDropDown {
 		Thread.sleep(2000);
 		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']"))
 				.click();
-		
+		Thread.sleep(2000);
 
+		driver.findElement(By.xpath("//*[contains(@class, 'ui-datepicker-days-cell-over') and contains(@class, 'ui-datepicker-today')]\r\n")).click();
+		
+		Assert.assertFalse(driver.findElement(By.id("ctl00_mainContent_view_date2")).isEnabled());
+		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
+		Assert.assertTrue(driver.findElement(By.id("ctl00_mainContent_view_date2")).isEnabled());
 	}
 
 }
