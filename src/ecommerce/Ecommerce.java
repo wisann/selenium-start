426b1f3cf350b399b4ee1b2ba56287ad9f967a0d
wisan <wisansilva3@gmail.com>
2024-06-24 16:09:10 -0300

    learning about implicit wait in selenium

--- src/ecommerce/Ecommerce.java
+++ src/ecommerce/Ecommerce.java
@@ -1,20 +1,35 @@
 package ecommerce;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Ecommerce {
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
-		int j = 0;
-		String[] itemsNeeded = { "Cucumber", "Brocolli" };
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		String[] itemsNeeded = { "Cucumber", "Brocolli", "Beetroot"};
 		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+		addItems(driver, itemsNeeded);
+		driver.findElement(By.cssSelector("img[alt='Cart']")).click();
+		driver.findElement(By.cssSelector("div[class='cart-preview active'] button[type='button']")).click();
+		String promoCode = "rahulshettyacademy";
+		driver.findElement(By.cssSelector("input[class='promoCode']")).sendKeys(promoCode);
+		driver.findElement(By.cssSelector("button[class='promoBtn']")).click();
+		System.out.println(driver.findElement(By.cssSelector("span[class='promoInfo']")).getText());
+	}
+
+	public static void addItems(WebDriver driver, String[] itemsNeeded) {
+		int j = 0;
 		List<WebElement> products = driver.findElements(By.cssSelector("h4.product-name"));
 		List itemsNeededList = Arrays.asList(itemsNeeded);
 		for (int i = 0; i < products.size(); i++) {
